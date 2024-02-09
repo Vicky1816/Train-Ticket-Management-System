71c614ef3cfd141bf4f2ce0d7f3a8f524a0357e2
Vivek Chaudhari <142868144+Vicky1816@users.noreply.github.com>
2024-02-09 20:44:35 +0530

    Update Footer.jsx

--- train-ticket-system-frontend-master/src/page/Footer.jsx
+++ train-ticket-system-frontend-master/src/page/Footer.jsx
@@ -7,11 +7,6 @@ const Footer = () => {
           <div class="text-center">
             Design and Developed by:<br></br>
             Vivek Vasudeo Chaudhari<br></br>
-            Akash Jagannath Ajabe<br></br>
-            Yash Bhagirath Pawar<br></br>
-            Pratik Sanjay Chitnis<br></br>
-            Priyanka Subhash Maindarkar<br></br>
-            Saurabh Prakash Patil<br></br>
             © 2023 Copyright
           </div>
         </footer>
